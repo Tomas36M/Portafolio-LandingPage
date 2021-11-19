7b4cd6c521353ec6daa7d7acfe956056a70788fd
Tomas36M <tomasmuenvar36@gmail.com>
2021-11-18 19:28:24 -0500

    first-commit

---
+++ src/sections/banner.js
@@ -0,0 +1,85 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
+import BannerImg from 'assets/profileImg.jpg';
+import ShapeLeft from 'assets/shape-left.png';
+import ShapeRight from 'assets/shape-right.png';
+
+export default function Banner() {
+  return (
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Tomas Munevar Escalante
+          </Heading>
+          <Text as="p" variant="heroSecondary" sx={{ textAlign: 'justify' }}>
+            Im a Full-Stack Developer, for more information about my experince, go to github and check out my portafolio.
+            Also if you are interesetd in my profile and how i am, go to my Linked In profile
+          </Text>
+          {/* <Button variant="primary">Explore</Button> */}
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="banner" />
+        </Box>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  banner: {
+    pt: ['140px', '145px', '155px', '170px', null, null, '180px', '215px'],
+    pb: [2, null, 0, null, 2, 0, null, 5],
+    position: 'relative',
+    zIndex: 2,
+    '&::before': {
+      position: 'absolute',
+      content: '""',
+      bottom: 6,
+      left: 0,
+      height: '100%',
+      width: '100%',
+      zIndex: -1,
+      backgroundImage: `url(${ShapeLeft})`,
+      backgroundRepeat: `no-repeat`,
+      backgroundPosition: 'bottom left',
+      backgroundSize: '36%',
+    },
+    '&::after': {
+      position: 'absolute',
+      content: '""',
+      bottom: '40px',
+      right: 0,
+      height: '100%',
+      width: '100%',
+      zIndex: -1,
+      backgroundImage: `url(${ShapeRight})`,
+      backgroundRepeat: `no-repeat`,
+      backgroundPosition: 'bottom right',
+      backgroundSize: '32%',
+    },
+    container: {
+      minHeight: 'inherit',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+    contentBox: {
+      width: ['100%', '90%', '535px', null, '90%', '90%', '90%', '90%'],
+      mx: 'auto',
+      textAlign: 'center',
+      mb: ['40px', null, null, null, null, 7],
+    },
+    imageBox: {
+      justifyContent: 'center',
+      textAlign: 'center',
+      display: 'inline-flex',
+      mb: [0, null, -6, null, null, '-40px', null, -3],
+      img: {
+        position: 'relative',
+        height: [150, 'auto'],
+      },
+    },
+  },
+};
